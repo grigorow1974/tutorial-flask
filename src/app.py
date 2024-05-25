40168aed4df49dc246ff84e009d6e30c49a7c29e
Sergio Grigorow <grigorow@gmail.com>
2024-05-25 16:39:32 -0400

    version3

--- src/app.py
+++ src/app.py
@@ -9,28 +9,20 @@ class_dict = {
     1: "No Adicto",
     }
 
-@app.route("/", methods = ["GET", "POST"])
+@app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
+        val1 = 1 if request.form["val1"] == "Yes" else 0
+        val2 = 1 if request.form["val2"] == "Yes" else 0
+        # Continuar con el resto de las variables...
         
-        val1 = request.form["val1"]
-        val2 = request.form["val2"]
-        val3 = request.form["val3"]
-        val4 = request.form["val4"]
-        val5 = request.form["val5"]
-        val6 = request.form["val6"]
-        val7 = request.form["val7"]
-        val8 = request.form["val8"]
-        val9 = request.form["val9"]
-        val10 = request.form["val10"]     
-
-        data = [[1 if val == "Yes" else 0 for val in [val1, val2, val3, val4, val5, val6, val7, val8, val9, val10]]]
+        data = [[val1, val2, val3, val4, val5, val6, val7, val8, val9, val10]]
         prediction = model.predict(data)[0]
         pred_class = class_dict[prediction]
     else:
         pred_class = None
     
-    return render_template("index.html", prediction = pred_class)
+    return render_template("index.html", prediction=pred_class)
 
 if __name__ == "__main__":
     app.run(debug=True)
