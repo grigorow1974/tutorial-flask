387fa9e356aca06577b168506980b1335ec2ad60
Sergio Grigorow <grigorow@gmail.com>
2024-05-25 17:19:18 -0400

    version 5

--- src/app.py
+++ src/app.py
@@ -5,8 +5,8 @@ from pickle import load
 app = Flask(__name__)
 model = load(open("best_model_random_forest", "rb"))
 class_dict = {
-    0: "Adicto",
-    1: "No Adicto",
+    1: "Adicto",
+    0: "No Adicto",
     }
 
 @app.route("/", methods=["GET", "POST"])
@@ -14,7 +14,15 @@ def index():
     if request.method == "POST":
         val1 = 1 if request.form["val1"] == "Yes" else 0
         val2 = 1 if request.form["val2"] == "Yes" else 0
-        # Continuar con el resto de las variables...
+        val3 = 1 if request.form["val3"] == "Yes" else 0
+        val4 = 1 if request.form["val4"] == "Yes" else 0
+        val5 = 1 if request.form["val5"] == "Yes" else 0
+        val6 = 1 if request.form["val6"] == "Yes" else 0
+        val7 = 1 if request.form["val7"] == "Yes" else 0
+        val8 = 1 if request.form["val8"] == "Yes" else 0
+        val9 = 1 if request.form["val9"] == "Yes" else 0
+        val10' = 1 if request.form["val10"] == "Yes" else 0
+        
         
         data = [[val1, val2, val3, val4, val5, val6, val7, val8, val9, val10]]
         prediction = model.predict(data)[0]
